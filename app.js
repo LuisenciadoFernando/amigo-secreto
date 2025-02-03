1df8d29e45bf6a7e3b6ac3c22246d4fc95098c1b
Fernando Padilla <luisenciadofernando@gmail.com>
2025-02-03 14:38:55 -0600

    Funcion mostrar lista amigos

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function agregarAmigo() {
         alert('Ingresa un nombre para continuar')
     } else {
     listadoAmigos.push(nombreIngresado); // ingresamos el valor a la lista
+    mostrarLista();
     console.log(listadoAmigos);
     vaciarInput();
     }
@@ -15,5 +16,14 @@ function agregarAmigo() {
 
 
 function vaciarInput() {
-    document.querySelector('#amigo').value = '';
+    document.querySelector('#amigo').value = ''; // colocar valor del input a vacío
+}
+
+
+function mostrarLista() {
+    let listaUL = document.querySelector('#listaAmigos'); // seleccionar el listado
+    listaUL.innerHTML = ''; // borrar datos previos antes de empezar
+    listadoAmigos.forEach(function(amigo) {
+        listaAmigos.innerHTML += `<li>${amigo}</li>`;
+    });
 }
